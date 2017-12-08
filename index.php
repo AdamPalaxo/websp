94688faa3b8194a176a2f77b3bd6c9488614833e
AdamPalaxo <awarus.trs@atlas.cz>
2017-12-08 10:39:58 +0100

    [Public] Index file updated.

--- index.php
+++ index.php
@@ -7,9 +7,13 @@ mb_internal_encoding("UTF-8");
 function autoload($class)
 {
     if (preg_match('/Controller$/', $class))
+    {
         require("controller/" . $class . ".php");
+    }
     else
+    {
         require("model/" . $class . ".php");
+    }
 }
 
 spl_autoload_register("autoload");
@@ -19,5 +23,4 @@ Db::connect("127.0.0.1", "kivweb", "root", "");
 $router = new RouterController();
 $router->process(array($_SERVER['REQUEST_URI']));
 
-$router->showView();
-
+$router->renderView();
